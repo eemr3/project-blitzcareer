c51e7a37b22675d9dca4533160fb1d7a89866600
eemr3 <eemr3@yahoo.com.br>
2022-06-25 20:31:27 -0300

    feat: :sparkles: User.js / src/tests/mocks
    adiciona mock para retorno de busca de todos os usuários

--- back-end/src/tests/mocks/User.js
+++ back-end/src/tests/mocks/User.js
@@ -4,6 +4,20 @@ const createUserMock = {
   password: '123456',
 };
 
+const findAllUserMOck = [
+  {
+    name: 'Emerson',
+    email: 'email@test.com',
+    password: '123456',
+  },
+  {
+    name: 'Pedro',
+    email: 'pedro@test.com',
+    password: '123456',
+  },
+];
+
 module.exports = {
   createUserMock,
+  findAllUserMOck,
 };
