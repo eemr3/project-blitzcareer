f4572e52aabfd683455447b33a79348913fc53d7
eemr3 <eemr3@yahoo.com.br>
2022-06-25 20:35:51 -0300

    feat: :sparkles: User.routes.js
    adiciona rota get "/users" que retornar todos os usuários

--- back-end/src/routes/User/User.routes.js
+++ back-end/src/routes/User/User.routes.js
@@ -4,5 +4,6 @@ const UserController = require('../../controllers/User.controller');
 const routes = express.Router();
 
 routes.post('/', UserController.createNewUser);
+routes.get('/', UserController.getAllUsers);
 
 module.exports = routes;
