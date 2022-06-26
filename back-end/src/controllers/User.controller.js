1c3e517e45897f1dd8b4e6ddcbd418edd8998200
eemr3 <eemr3@yahoo.com.br>
2022-06-25 21:10:49 -0300

    feat: :sparkles: User.controller.js
    adiciona middleware reponsavel por realizar consulta ao bando de dados buscando um usuário pelo seu id, acessando atraves do service

--- back-end/src/controllers/User.controller.js
+++ back-end/src/controllers/User.controller.js
@@ -7,13 +7,25 @@ const createNewUser = async (req, res) => {
   return res.status(201).json(user);
 };
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (_req, res) => {
   const users = await UserService.getAllUsers();
 
   return res.status(200).json(users);
 };
 
+const getByIdUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserService.getByIdUser(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createNewUser,
   getAllUsers,
+  getByIdUser,
 };
