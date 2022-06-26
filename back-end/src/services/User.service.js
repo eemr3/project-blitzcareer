c23ae807c29a59769014aedd6ee9625a78867a07
eemr3 <eemr3@yahoo.com.br>
2022-06-25 21:07:21 -0300

    feat: :sparkles: User.service.js
    adiciona função getByIdUser que tem a resposabilidade de realizar consulta no banco de dados para retorno de um usuário

--- back-end/src/services/User.service.js
+++ back-end/src/services/User.service.js
@@ -7,12 +7,24 @@ const createNewUser = async (name, email, password) => {
 };
 
 const getAllUsers = async () => {
-  const users = await User.findAll();
+  const users = await User.findAll({ attributes: { exclude: ['password'] } });
 
   return users;
+};
+
+const getByIdUser = async (id) => {
+  const user = await User.findOne({
+    where: id,
+    attributes: { exclude: ['password'] },
+  });
+
+  if (!user) throw new Error('User not foud');
+
+  return user;
 };
 
 module.exports = {
   createNewUser,
   getAllUsers,
+  getByIdUser,
 };
