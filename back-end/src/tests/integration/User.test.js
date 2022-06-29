d56a4cfeefc076018b1130a9fc84f84a9bb42bf9
eemr3 <eemr3@yahoo.com.br>
2022-06-28 23:27:37 -0300

    refactor: User.test.js
    refatora teste da rota login it "ao efetuar login com sucesso

--- back-end/src/tests/integration/User.test.js
+++ back-end/src/tests/integration/User.test.js
@@ -31,7 +31,13 @@ describe('User', () => {
   describe('metodo "GET"', () => {
     describe('rota "/users"', () => {
       it('busca todos os usuários cadastrados', async () => {
-        chaiHttpResponse = await chai.request(app).get('/users');
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/users')
+          .set(
+            'authorization',
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoiRW1lcnNvbiBNb3JlaXJhIiwiZW1haWwiOiJlZW1yM0BlbWFpbC5jb20ifSwiaWF0IjoxNjU2NDY3NTg2LCJleHAiOjE2NTkwNTk1ODZ9.ANzAkwFwkUmkYLvS-XmoCSZoKqMYbcnjkv8v0PDRL4s',
+          );
 
         expect(chaiHttpResponse.status).to.be.equal(200);
         for (let index = 0; index < chaiHttpResponse.body.length; index += 1) {
@@ -45,13 +51,20 @@ describe('User', () => {
 
     describe('rota "/users/:id"', () => {
       it('busca todos os usuários cadastrados', async () => {
-        chaiHttpResponse = await chai.request(app).get('/users/1');
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/users/1')
+          .set(
+            'authorization',
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoiRW1lcnNvbiBNb3JlaXJhIiwiZW1haWwiOiJlZW1yM0BlbWFpbC5jb20ifSwiaWF0IjoxNjU2NDY3NTg2LCJleHAiOjE2NTkwNTk1ODZ9.ANzAkwFwkUmkYLvS-XmoCSZoKqMYbcnjkv8v0PDRL4s',
+          );
 
         expect(chaiHttpResponse.status).to.be.equal(200);
 
         expect(chaiHttpResponse.body).to.have.property('id');
         expect(chaiHttpResponse.body).to.have.property('name');
         expect(chaiHttpResponse.body).to.have.property('email');
+        expect(chaiHttpResponse.body).to.have.property('toDos');
         expect(chaiHttpResponse.body).to.not.have.property('password');
       });
     });
