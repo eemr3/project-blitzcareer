5001fff9b7d05d816c8c5689f9897a0ab6a7a960
eemr3 <eemr3@yahoo.com.br>
2022-06-29 12:03:08 -0300

    style: :lipstick: Header/index.js
    adiciona estilo utilizando o bootstrap

--- frot-end/src/components/Header/index.js
+++ frot-end/src/components/Header/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div>
-      <h1>Lista de tarefas - Ebytr</h1>
-    </div>
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>
+          <h1> Lista de tarefas - Ebytr</h1>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
