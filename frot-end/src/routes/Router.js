c32b642f7a22fc795462241335136f723367e30b
eemr3 <eemr3@yahoo.com.br>
2022-06-29 11:35:08 -0300

    feat: :sparkles: Router.js
    adiciona rota "create-user" para a página NewUser

--- frot-end/src/routes/Router.js
+++ frot-end/src/routes/Router.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
+import NewUser from '../pages/NewUser';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
+      <Route path='/create-user' element={<NewUser />} />
     </Routes>
   );
 };
