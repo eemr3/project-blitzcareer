c3e3efee70747e2fd6dc9246fcc23ce537476c5a
eemr3 <eemr3@yahoo.com.br>
2022-06-29 12:03:49 -0300

    style: :lipstick: Login/index.js
    adiciona estilo utilizando o bootstrap

--- frot-end/src/pages/Login/index.js
+++ frot-end/src/pages/Login/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function Login() {
   return (
-    <div>
+    <>
       <Header />
-      <div>
-        <input type='text' placeholder='E-mail' />
-        <input type='text' placeholder='Password' />
-        <button type='button'>Entrar</button>
-      </div>
-      <div>
-        <p>Não tem uma conta?</p>
-        <Link to='/create-user'>Crie agora</Link>
-      </div>
-    </div>
+      <Container>
+        <h1 style={{ textAlign: 'center' }}>Login</h1>
+        <Form>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Usuário</Form.Label>
+            <Form.Control type='email' placeholder='E-mail' />
+            <Form.Text className='text-muted'></Form.Text>
+          </Form.Group>
+
+          <Form.Group className='mb-3' controlId='formBasicPassword'>
+            <Form.Label>Senha</Form.Label>
+            <Form.Control type='password' placeholder='Senha' />
+          </Form.Group>
+          <Form.Group
+            className='mb-3'
+            controlId='formBasicCheckbox'></Form.Group>
+          <Button variant='primary' type='submit'>
+            Entrar
+          </Button>
+        </Form>
+        <div style={{ display: 'flex', marginTop: '15px' }}>
+          <p style={{ marginRight: '7px' }}>Não tem uma conta?</p>
+
+          <Link to='/create-user'>Crie agora</Link>
+        </div>
+      </Container>
+    </>
   );
 }
 
