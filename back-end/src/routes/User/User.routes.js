5b142dbc6ee8635581b882a3d025371486b8ad20
eemr3 <eemr3@yahoo.com.br>
2022-06-28 22:38:34 -0300

    feat: :sparkles: User.routes.js
    adiciona o middleware de autenticação aos end-points "/users" e "users/id"

--- back-end/src/routes/User/User.routes.js
+++ back-end/src/routes/User/User.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const UserController = require('../../controllers/User.controller');
+const authMiddleware = require('../../middleware/AuthMiddleware');
 
 const routes = express.Router();
 
-routes.get('/:id', UserController.getByIdUser);
+routes.get('/:id', authMiddleware, UserController.getByIdUser);
+routes.get('/', authMiddleware, UserController.getAllUsers);
 routes.post('/', UserController.createNewUser);
-routes.get('/', UserController.getAllUsers);
 
 module.exports = routes;
