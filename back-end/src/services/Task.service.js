36e743e28939ab5d41cf8a1706b36d26ee53752c
eemr3 <eemr3@yahoo.com.br>
2022-06-29 00:06:15 -0300

    feat: :sparkles: Task.service.js
    adiciona função updateTask que realiza o update no banco de dados de toDos

--- back-end/src/services/Task.service.js
+++ back-end/src/services/Task.service.js
@@ -19,8 +19,28 @@ const getTaskById = async (id) => {
 
   return task;
 };
+
+const updateTask = async ({ title, description, status, id }) => {
+  const idTask = Number(id);
+  const taskExist = await getTaskById(idTask);
+  if (!taskExist) {
+    throw new Error('Task not found');
+  }
+  const taskUpdated = await ToDo.update(
+    { title, description, status },
+    { where: { id: idTask } },
+  );
+
+  if (taskUpdated === 0) {
+    throw new Error('Error updated');
+  }
+  const task = await getTaskById(idTask);
+  return task;
+};
+
 module.exports = {
   createTask,
   getAllTasks,
   getTaskById,
+  updateTask,
 };
