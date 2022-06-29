7e53fc6697b46934f244e2aa69a6153fe4f11418
eemr3 <eemr3@yahoo.com.br>
2022-06-28 23:38:50 -0300

    feat: :sparkles: Task.routes.js
    adiciona rota metodo get "/" e get"/:id"

--- back-end/src/routes/Task/Task.routes.js
+++ back-end/src/routes/Task/Task.routes.js
@@ -5,6 +5,8 @@ const { taskMiddleware } = require('../../middleware/TaskMiddleware');
 
 const routes = express.Router();
 
+routes.get('/:id', authMiddleware, TaskController.getTaskById);
+routes.get('/', authMiddleware, TaskController.getAllTasks);
 routes.post('/', authMiddleware, taskMiddleware, TaskController.createTask);
 
 module.exports = routes;
