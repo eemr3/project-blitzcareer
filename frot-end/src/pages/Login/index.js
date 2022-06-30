d18fa744e2ad5d3d558d2e4877e259b03beb9006
eemr3 <eemr3@yahoo.com.br>
2022-06-30 12:12:38 -0300

    feat: :sparkles: Login/index.js
    adiciona chamada ao end-point "/login" para efetuar o logina na plicação usando o axios

--- frot-end/src/pages/Login/index.js
+++ frot-end/src/pages/Login/index.js
@@ -1,29 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../../api/api';
 import styles from '../../styles/pages/Login.module.css';
 
 function Login() {
+  const navigate = useNavigate();
+  const [inputLogin, setInputLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await api.post('/login', inputLogin);
+
+      localStorage.setItem('token', result.data.token);
+      navigate('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={ styles.container }>
 
       <h1 className={ styles.title }>Login</h1>
-      <Form>
+      <Form onSubmit={ handleSubmit }>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Usuário</Form.Label>
-          <Form.Control type="email" placeholder="E-mail" />
+          <Form.Control
+            required
+            type="email"
+            placeholder="E-mail"
+            value={ inputLogin.email }
+            onChange={ (event) => setInputLogin({
+              ...inputLogin, email: event.target.value,
+            }) }
+          />
           <Form.Text className="text-muted" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" placeholder="Senha" />
+          <Form.Control
+            required
+            type="password"
+            value={ inputLogin.password }
+            placeholder="Senha"
+            onChange={ (event) => setInputLogin({
+              ...inputLogin, password: event.target.value,
+            }) }
+          />
         </Form.Group>
         <Form.Group
           className="mb-3"
           controlId="formBasicCheckbox"
         />
-        <Button variant="primary" type="submit">
+        <Button
+          variant="primary"
+          type="submit"
+        >
           Entrar
         </Button>
       </Form>
