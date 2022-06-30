48abd99fd0f0bf9df95c0cc84d8dd5689ff141ae
eemr3 <eemr3@yahoo.com.br>
2022-06-30 17:20:39 -0300

    corige alguns erros de escrita

--- back-end/src/tests/integration/Login.test.js
+++ back-end/src/tests/integration/Login.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const { timeout } = require('frisby');
 const Response = require('superagent');
 
 chai.use(chaiHttp);
@@ -7,13 +8,18 @@ chai.use(chaiHttp);
 const { expect } = chai;
 const app = require('../../api/app');
 const connection = require('../../database/models');
-const { loginUserMock, loginUserErrorMock } = require('../mocks/User');
-const truncate = require('./truncate');
+const { loginUserMock } = require('../mocks/User');
+// const truncate = require('./truncate');
 
 describe('Login', () => {
   let chaiHttpResponse = Response;
 
   describe('rota "/login"', () => {
+    beforeEach(async (done) => {
+      timeout(10000)
+      done();
+    });
+
     it('ao efetuar o login com sucesso', async () => {
       chaiHttpResponse = await chai
         .request(app)
@@ -22,18 +28,7 @@ describe('Login', () => {
 
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.have.property('token');
+      
     });
-
-    // it('ao efetuar o login sem sucesso', async () => {
-    //   chaiHttpResponse = await chai
-    //     .request(app)
-    //     .post('/login')
-    //     .send(loginUserErrorMock);
-
-    //   expect(chaiHttpResponse.status).to.be.equal(401);
-    //   expect(chaiHttpResponse.body.message).to.be.equal(
-    //     'Incorrect email or password',
-    //   );
-    // });
   });
 });
