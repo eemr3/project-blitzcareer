3335bd69b8aeed7a1909ff973a5c6aa58b5c9b25
eemr3 <eemr3@yahoo.com.br>
2022-06-30 12:16:19 -0300

    feat: :sparkles: Home/components/TableComponent/index.js
    adiciona chamada ao end-point "/users/userid" para renderizar tasks relativa ao usuário logado
    
    acessado o end-point "/tasks" metodo delete para apagar uma task

--- frot-end/src/pages/Home/componets/Table/index.js
+++ frot-end/src/pages/Home/componets/Table/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import api from '../../../../api/api';
+import Context from '../../../../context/Context';
 import styles from '../../../../styles/components/Table.module.css';
 
 function TableComponent() {
+  const { setNameUser, isSave, setIsSave } = useContext(Context);
+  const [dataToDoUser, setDataToDoUser] = useState([]);
+  const tokenLS = localStorage.getItem('token') || '';
+
+  useEffect(() => {
+    const getDataUserApi = async () => {
+      const resultTaks = await api.get('/users/userid', {
+        headers: { Authorization: tokenLS },
+      });
+      const { id, name } = resultTaks.data;
+      setDataToDoUser(resultTaks.data.toDos);
+      setNameUser({ id, name });
+    };
+
+    getDataUserApi();
+  }, [isSave, setNameUser, tokenLS]);
+
+  const handleDeleteTask = async (id) => {
+    const resultDelete = await api.delete(`/tasks/${id}`, {
+      headers: { Authorization: tokenLS },
+    });
+
+    setIsSave(!isSave);
+    console.log(resultDelete);
+  };
+
   return (
     <Table striped bordered hover variant="dark">
       <thead>
@@ -14,45 +42,31 @@ function TableComponent() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Ir ao supermercado</td>
-          <td>Realizar compras do mês e compras de pneus para o carro</td>
-          <td>pendente</td>
-          <td className={ styles.tdContent }>
-            <Button type="button" variant="secondary">
-              <i className="bi bi-pencil-square" />
-            </Button>
-            <Button type="button" variant="danger">
-              <i className="bi bi-trash" />
-            </Button>
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td className={ styles.tdContent }>
-            <Button type="button" variant="secondary">
-              <i className="bi bi-pencil-square" />
-            </Button>
-            <Button type="button" variant="danger">
-              <i className="bi bi-trash" />
-            </Button>
-          </td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Larry the Bird</td>
-          <td>@twitter</td>
-          <td className={ styles.tdContent }>
-            <Button type="button" variant="secondary">
-              <i className="bi bi-pencil-square" />
-            </Button>
-            <Button type="button" variant="danger">
-              <i className="bi bi-trash" />
-            </Button>
-          </td>
-        </tr>
+        {dataToDoUser.map((task) => (
+          <tr key={ task.id }>
+            <td>{task.title}</td>
+            <td>{task.description}</td>
+            <td>{task.status}</td>
+            <td className={ styles.tdContent }>
+              <Button
+                type="button"
+                variant="secondary"
+                disabled={ task.status === 'Pronto' }
+                onClick={ () => { console.log('Oi'); } }
+              >
+                <i className="bi bi-pencil-square" />
+              </Button>
+              <Button
+                onClick={ () => handleDeleteTask(task.id) }
+                type="button"
+                variant="danger"
+              >
+                <i className="bi bi-trash" />
+              </Button>
+            </td>
+          </tr>
+
+        ))}
       </tbody>
     </Table>
   );
