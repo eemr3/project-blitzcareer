f721af4ba666b11a218270d6cece8dbbc00beea7
eemr3 <eemr3@yahoo.com.br>
2022-06-29 21:41:16 -0300

    refactor: remove o componete Header pois não fazia sentido estar nas duas páginas

--- frot-end/src/pages/Login/index.js
+++ frot-end/src/pages/Login/index.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Header from '../../components/Header';
+import styles from '../../styles/pages/Login.module.css';
 
 function Login() {
   return (
-    <>
-      <Header />
-      <Container>
-        <h1 style={{ textAlign: 'center' }}>Login</h1>
-        <Form>
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>Usuário</Form.Label>
-            <Form.Control type='email' placeholder='E-mail' />
-            <Form.Text className='text-muted'></Form.Text>
-          </Form.Group>
+    <div className={ styles.container }>
 
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type='password' placeholder='Senha' />
-          </Form.Group>
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicCheckbox'></Form.Group>
-          <Button variant='primary' type='submit'>
-            Entrar
-          </Button>
-        </Form>
-        <div style={{ display: 'flex', marginTop: '15px' }}>
-          <p style={{ marginRight: '7px' }}>Não tem uma conta?</p>
+      <h1 className={ styles.title }>Login</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Usuário</Form.Label>
+          <Form.Control type="email" placeholder="E-mail" />
+          <Form.Text className="text-muted" />
+        </Form.Group>
 
-          <Link to='/create-user'>Crie agora</Link>
-        </div>
-      </Container>
-    </>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicCheckbox"
+        />
+        <Button variant="primary" type="submit">
+          Entrar
+        </Button>
+      </Form>
+      <div style={ { display: 'flex', marginTop: '15px' } }>
+        <p style={ { marginRight: '7px' } }>Não tem uma conta?</p>
+
+        <Link to="/create-user">Crie agora</Link>
+      </div>
+
+    </div>
   );
 }
 
--- frot-end/src/pages/NewUser/index.js
+++ frot-end/src/pages/NewUser/index.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import Header from '../../components/Header';
+import { Button, Form } from 'react-bootstrap';
+import styles from '../../styles/pages/NewUser.module.css';
 
 function NewUser() {
   return (
-    <>
-      <Header />
-      <Container>
-        <Form>
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>Nome</Form.Label>
-            <Form.Control type='text' placeholder='Nome completo' />
-            <Form.Text className='text-muted'></Form.Text>
-          </Form.Group>
+    <div className={ styles.container }>
+      <h1 className={ styles.title }>Criar novo usuário</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nome</Form.Label>
+          <Form.Control type="text" placeholder="Nome completo" />
+          <Form.Text className="text-muted" />
+        </Form.Group>
 
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>E-mail</Form.Label>
-            <Form.Control type='email' placeholder='E-mail' />
-          </Form.Group>
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type='password' placeholder='Senha' />
-          </Form.Group>
-          <Button variant='primary' type='submit'>
-            Criar conta
-          </Button>
-        </Form>
-      </Container>
-    </>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control type="email" placeholder="E-mail" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Criar conta
+        </Button>
+      </Form>
+    </div>
   );
 }
 
