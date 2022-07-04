26714a3ef27ac095f84991bfedaf6d1361e472b0
eemr3 <eemr3@yahoo.com.br>
2022-07-04 17:15:07 -0300

    test: NewUser.test.js
    cria tests para a página de novo usuário

---
+++ front-end/src/tests/NewUser.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NewUser from '../pages/NewUser';
+
+describe('Tela de New User', () => {
+  it('renderiza titulo na tela', () => {
+    renderWithRouter(<NewUser />);
+    const title = screen.getByText('Criar novo usuário');
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('renderiza inputs na tela', () => {
+    renderWithRouter(<NewUser />);
+    const inputNome = screen.getByPlaceholderText('Nome completo');
+    const inputEmail = screen.getByPlaceholderText('E-mail');
+    const inputPassword = screen.getByPlaceholderText('Senha');
+
+    expect(inputNome).toHaveValue('');
+    expect(inputNome).toBeInTheDocument();
+    userEvent.type(inputNome, 'Ichigo');
+    expect(inputNome).toHaveValue('Ichigo');
+
+    expect(inputEmail).toHaveValue('');
+    expect(inputEmail).toBeInTheDocument();
+    userEvent.type(inputEmail, 'ichigo@gmail.com');
+    expect(inputEmail).toHaveValue('ichigo@gmail.com');
+
+    expect(inputPassword).toHaveValue('');
+    expect(inputPassword).toBeInTheDocument();
+    expect(inputPassword).toHaveAttribute('type', 'password');
+  });
+});
