d47f6cb7589b334ba0b246ce8b3e8fcde6170503
Emerson <eemr3@yahoo.com.br>
2023-02-07 15:10:18 -0300

    test: ensure SinUpController returns 500 if EmailValidator throw erros

--- back-end/src/presentation/controllers/signup/signup.controller.spec.ts
+++ back-end/src/presentation/controllers/signup/signup.controller.spec.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../../errors/invalid-param.error';
 import { MissingParamError } from '../../errors/missing-param.error';
+import { ServerError } from '../../errors/server.error copy';
 import { EmailValidator } from '../../protocols/email-validator';
 import { SignUpController } from './signup.controller';
 
@@ -115,5 +116,28 @@ describe('Signup Controller', () => {
 
     sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
+
+  it('should return 500 if EmailValidatore throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_passoerd',
+        passwordConfirmation: 'any_passoerd',
+      },
+    };
+
+    const response = sut.handle(httpRequest);
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual(new ServerError());
   });
 });
