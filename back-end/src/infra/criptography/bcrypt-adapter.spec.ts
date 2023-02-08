be499a89f393c990f6905febf1db0593d3f226b2
Emerson <eemr3@yahoo.com.br>
2023-02-08 11:51:58 -0300

    test: ensure BcryptAdapter return a hash on success

--- back-end/src/infra/criptography/bcrypt-adapter.spec.ts
+++ back-end/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,11 @@
 import * as bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
+
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('hash'));
+  },
+}));
 describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct value', async () => {
     const salt = 12;
@@ -9,5 +15,13 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_password');
 
     expect(hashSpy).toHaveBeenCalledWith('any_password', salt);
+  });
+
+  it('should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hash = await sut.encrypt('any_password');
+
+    expect(hash).toBe('hash');
   });
 });
