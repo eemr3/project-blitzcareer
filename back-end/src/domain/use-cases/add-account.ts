290dd524afe17a80bb2e7dbfec0a94c534f02e2e
Emerson <eemr3@yahoo.com.br>
2023-02-08 10:10:16 -0300

    feat: ensure DbAddAccount call Encrypter with correct password

--- back-end/src/domain/use-cases/add-account.ts
+++ back-end/src/domain/use-cases/add-account.ts
@@ -7,5 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-  add(accoutn: AddAccountModel): Promise<AccountModel>;
+  add(account: AddAccountModel): Promise<AccountModel>;
 }
