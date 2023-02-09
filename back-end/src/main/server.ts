35945b7d23c6f59a250031289fa436f5afa99f57
Emerson <eemr3@yahoo.com.br>
2023-02-09 19:28:21 -0300

    feat: add express and config

---
+++ back-end/src/main/server.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => res.send('Inicio de uma jornada :)'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
