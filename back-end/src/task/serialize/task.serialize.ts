c8c78f38ab7c49f595f7bc779a1b0df58d5b0a1e
Emerson Moreira <eemr3@yahoo.com.br>
2023-06-30 16:36:09 -0300

    feat: create serialize task

---
+++ back-end/src/task/serialize/task.serialize.ts
@@ -0,0 +1,18 @@
+export class TaskSerialize {
+  dbToResponseGet(task: any) {
+    console.log(task);
+    return {
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      User: {
+        userId: task.userId,
+        name: task.User.name,
+        email: task.User.email,
+      },
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    };
+  }
+}
