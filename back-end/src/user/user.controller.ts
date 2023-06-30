00e4b6371643ab466fce35fa6406278df1f4dd21
Emerson Moreira <eemr3@yahoo.com.br>
2023-06-30 11:57:57 -0300

    feat: add end-point findById, getMe and findAll in controller user

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import { UserService } from './user.service';
 import { ConflictException } from '../shared/exceptions/conflict.exception';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { NotFoundException } from '../shared/exceptions/not-found.exception';
+import { RequestWithUser } from '../authentication/middleware/auth.middleware';
 
 export class UserController {
   constructor(private userService: UserService) {}
@@ -20,6 +22,57 @@ export class UserController {
           statusCode: 409,
           message: error.message,
           error: 'Conflict',
+        });
+      }
+
+      return res.status(500).json({
+        statusCode: 500,
+        message: 'Internal server error',
+      });
+    }
+  }
+
+  async findAll(req: Request, res: Response) {
+    return res.status(200).json(await this.userService.findAll());
+  }
+
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const user = await this.userService.findOne(Number(id));
+      return res.status(200).json(user);
+    } catch (err) {
+      const error = err as Error;
+
+      if (err instanceof NotFoundException) {
+        return res.status(404).json({
+          statusCode: 404,
+          message: error.message,
+          error: 'Not Found',
+        });
+      }
+
+      return res.status(500).json({
+        statusCode: 500,
+        message: 'Internal server error',
+      });
+    }
+  }
+
+  async getMe(req: RequestWithUser, res: Response) {
+    const sub = req.user?.sub;
+    try {
+      const user = await this.userService.findOne(Number(sub));
+      return res.status(200).json(user);
+    } catch (err) {
+      const error = err as Error;
+
+      if (err instanceof NotFoundException) {
+        return res.status(404).json({
+          statusCode: 404,
+          message: error.message,
+          error: 'Not Found',
         });
       }
 
