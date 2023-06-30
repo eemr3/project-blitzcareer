ee39b08b39b0817995a4f74313398fad1ee473f4
Emerson Moreira <eemr3@yahoo.com.br>
2023-06-30 11:55:32 -0300

    feat: add routes auth

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,8 @@
 import express, { Application } from 'express';
-import { UserRouter } from './infra/routes';
+import { UserRouter, AuthRoutes } from './infra/routes';
 class App {
   public express: Application;
+  private authRoutes = new AuthRoutes();
   private userRoutes = new UserRouter();
 
   constructor() {
@@ -14,6 +15,7 @@ class App {
     this.express.get('/', (req: any, res: any) => {
       res.send('Hello World');
     });
+    this.express.use(this.authRoutes.routes);
     this.express.use(this.userRoutes.routes);
   }
 }
