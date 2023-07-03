0ffc8a19c6a5b8b15bd4bc24e74eb036f331a6b7
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-03 17:24:24 -0300

    feat: add config swagger

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,27 +1,30 @@
 import cors from 'cors';
 import express, { Application } from 'express';
+import swagerUi from 'swagger-ui-express';
 import { UserRouter, AuthRoutes, TaskRoutes } from './infra/routes';
+import * as swaggerDocument from './swagger.json';
 class App {
-  public express: Application;
+  public httpServer: Application;
   private authRoutes = new AuthRoutes();
   private userRoutes = new UserRouter();
   private taskRoutes = new TaskRoutes();
 
   constructor() {
-    this.express = express();
-    this.express.use(cors());
-    this.express.use(express.json());
+    this.httpServer = express();
+    this.httpServer.use(cors());
+    this.httpServer.use(express.json());
+    this.httpServer.use('/docs', swagerUi.serve, swagerUi.setup(swaggerDocument));
     this.routes();
   }
 
   private routes() {
-    this.express.get('/', (req: any, res: any) => {
+    this.httpServer.get('/', (req: any, res: any) => {
       res.send('Hello World');
     });
-    this.express.use(this.authRoutes.routes);
-    this.express.use(this.userRoutes.routes);
-    this.express.use(this.taskRoutes.routes);
+    this.httpServer.use(this.authRoutes.routes);
+    this.httpServer.use(this.userRoutes.routes);
+    this.httpServer.use(this.taskRoutes.routes);
   }
 }
 
-export default new App().express;
+export default new App().httpServer;
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import App from './app';
 
-const port = process.env.PORT || 3333;
+const port = parseInt(process.env.PORT || '3333');
 
 App.listen(port, () => {
   console.log(`Server runing at http://localhost:${port}`);
