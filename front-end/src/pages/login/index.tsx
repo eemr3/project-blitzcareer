f1d97c3f7ea83ec43d758d112165c1f4e8f67c2f
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-08 23:18:37 -0300

    feat: cria a página de Login

---
+++ front-end/src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '../../components/LoginForm';
+import LoginAndRegister from '../../components/template/LoginAndRegister';
+
+export default function Login() {
+  return (
+    <>
+      <LoginAndRegister>
+        <LoginForm />
+      </LoginAndRegister>
+    </>
+  );
+}
