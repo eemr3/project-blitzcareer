3f7e304dd849169d8023ecdab2109f8f8ce3eb7a
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-08 23:14:06 -0300

    feat: cria o component Column

---
+++ front-end/src/components/Column/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ICard } from '../Board';
+import Card from '../Card';
+
+type ColumnProps = {
+  title: string;
+  status: string;
+  cards: ICard[];
+  onDrop: (event: React.DragEvent<HTMLDivElement>, newStatus: string) => void;
+  onDragStart: (event: React.DragEvent<HTMLDivElement>, cardId: string) => void;
+};
+
+const Column: React.FC<ColumnProps> = ({ title, status, cards, onDrop, onDragStart }) => {
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      className={`flex flex-col bg-gray-700 min-h-[212px] w-[320px] rounded-lg p-4`}
+      onDragOver={handleDragOver}
+      onDrop={(event) => onDrop(event, status)}
+    >
+      <h2 className="text-2xl font-bold mb-4 capitalize text-white">{title}</h2>
+      {cards.map((card) => (
+        <Card key={card.id} card={card} onDragStart={onDragStart} />
+      ))}
+    </div>
+  );
+};
+
+export default Column;
