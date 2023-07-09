880537d37acf5ebc4c40f0b9f6d3b5fc901fee63
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-08 23:19:15 -0300

    feat: cria a página de registro de usuário

---
+++ front-end/src/pages/register-account/index.tsx
@@ -0,0 +1,10 @@
+import LoginAndRegister from '../../components/template/LoginAndRegister';
+import { RegisterForm } from '../../components/RegisterForm';
+
+export default function RegisterAccount() {
+  return (
+    <LoginAndRegister>
+      <RegisterForm />
+    </LoginAndRegister>
+  );
+}
