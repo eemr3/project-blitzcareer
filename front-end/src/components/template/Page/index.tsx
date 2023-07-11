536ccabc87ad879c8285fd1ccabb0a2983e8f7a2
Emerson Moreira <eemr3@yahoo.com.br>
2023-07-11 16:58:09 -0300

    feat: cria modal para confirmação de exclusão de tarefa

--- front-end/src/components/template/Page/index.tsx
+++ front-end/src/components/template/Page/index.tsx
@@ -9,6 +9,11 @@ export default function Page({ children, className }: PageProps) {
   return (
     <div className={`w-full min-h-screen bg-gray-50 dark:bg-gray-900 ${className}`}>
       {children}
+      <footer className="fixed bottom-0 left-0 h-10 w-full">
+        <p className="text-center text-gray-50">
+          Todos os direitos reservados &copy; Emerson Moirera
+        </p>
+      </footer>
     </div>
   );
 }
